e4b1a4fea21c08d9982af5074b7628466cbc9c10
Noy <60noypearl@gmail.com>
2017-07-25 16:57:22 +0300

    fetching rests data

--- app/views/Home/Home.js
+++ app/views/Home/Home.js
@@ -1,8 +1,12 @@
+/* eslint-disable camelcase */
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+import axios from 'axios'
 import { Slider, Button } from 'react-native-elements'
 import LoaderSpin from 'react-native-spinkit'
 import { red } from '../../utils/colors'
+import { NEARBY_PLACES_BASE_URL, PLACE_PHOTO_BASE_URL, PLACE_DETAILS_BASE_URL } from '../../utils/constants'
 // TODO: user palette from material colors
 // TODO: add status - e.g 'you must be hungry' for 1km distance
 
@@ -40,18 +44,71 @@ class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      distance: 4,
+      distance: 10,
       isLoading: false,
+      test: 'test',
+      latitude: 4,
+      longitude: 0,
+      error: null,
     }
   }
+  componentDidMount() {
+    navigator.geolocation.watchPosition(
+      (position) => {
+        console.log(`okay${position}`)
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        })
+      },
+      error => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+    )
+  }
+  // updates distance state on slider value change
   handleSliderChange = (distance) => {
     this.setState({ distance })
   }
 
+  //
   searchRestaurants = () => {
+    const { distance, latitude, longitude } = this.state
+    const radius = distance * 1000 // 1km = 1000m
+    const url = `${NEARBY_PLACES_BASE_URL}&radius=${radius}&location=${latitude},${longitude}`
+    console.log(url)
+    axios.get(url)
+    .then((response) => { this.openCardsPage(response.data) })
+    .catch((error) => { console.error(error) })
     // call fetch
     // push router with props of restaurants
     this.setState({ isLoading: true })
+  }
+  openCardsPage = (data) => {
+    console.log(`data: ${JSON.stringify(data)}`)
+    const results = data.results.map(item => ({
+      id: item.place_id,
+      // image: this.photoDataToImage(item.photos[0].image).then(image => image),
+      // TODO: return the data from image promise after uncommit
+      imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipOQeNnuchvBNedMBGv8qysC__il-UYEZFWI23WQ=s1600-w400',
+      name: item.name,
+      rating: item.rating,
+      types: item.types,
+    }))
+    // debugger // eslint-disable-line no-debugger
+    this.setState({ isLoading: false, test: `${results[0]}a` })
+    Actions.cards(results)
+  }
+
+  restDataToRestDetailsPromise = restData => axios.get(`${PLACE_DETAILS_BASE_URL}&place_id=${restData.place_id}`)
+    .catch((error) => { this.setState({ error }) })
+  // photo data object to image
+  photoDataToImage = (photo) => {
+    const { width, height, photo_reference } = photo
+    const url = `${PLACE_PHOTO_BASE_URL}&maxwidth=${width}&maxheight=${height}&photoreference=${photo_reference}`
+    return axios.get(url)
+    .then(img => img)
+    .catch((err) => { this.setState({ error: `error fetching image${err}` }) })
   }
   render() {
     const { distance, isLoading } = this.state
@@ -60,6 +117,12 @@ class Home extends Component {
         <Text style={styles.primaryTitle}>
         Feed Me
       </Text>
+        <Text>
+          {this.state.test}
+        </Text>
+        <Text>
+          latitude : {this.state.latitude} longitude: {this.state.longitude} {this.state.error}
+        </Text>
         <Text style={styles.sencondaryTitle}>
         How far will you go?
       </Text>
--- app/views/RestaurantCards/RestaurantCards.js
+++ app/views/RestaurantCards/RestaurantCards.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 const RestaurantCards = () => (
-  <View />
+  <View>
+    <CardsDector>
+      {data.map((card) =>
+        <Card
+          name={card.name}
+          imageUrl={card.imageUrl}
+          rating={card.rating}
+          p
+      )}
+    </CardsDector>
+    <Text />
+  </View>
+  // <CardView
+  //   image={cards.image}
+  //   title={cards.title}
+  //   desciption={cards.description}
+  // />
 )
 
 export default RestaurantCards
